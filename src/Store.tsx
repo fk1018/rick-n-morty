dbbc65d1b6ddf5c4daa0206fe7bab8dbbee98824
Fred Khoury <fredrick.khoury@hexagonsi.com>
2019-12-31 11:57:39 -0600

    zz

---
+++ src/Store.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+interface IState {
+	episodes: [];
+	favorites: [];
+}
+
+interface IAction {
+	type: string;
+	payload: any;
+}
+
+const initialState: IState = {
+	episodes: [],
+	favorites: []
+};
+
+export const Store = React.createContext<IState>(initialState);
+
+function reducer(state: IState, action: IAction): IState {
+	switch (action.type) {
+		case 'FETCH_DATA':
+			return { ...state, episodes: action.payload };
+		default:
+			return state;
+	}
+}
+
+export function StoreProvider(props: any): JSX.Element {
+	return <Store.Provider value={initialState}>{props.children}</Store.Provider>;
+}
